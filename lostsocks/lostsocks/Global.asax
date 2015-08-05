1640518efccb74afd3bd36003fa0c4ac0fff0b1d
Todd B. Adams <toddbadams@msn.com>
2015-08-05 13:15:49 +0100

    Added Lost Socks

---
+++ lostsocks/lostsocks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lostsocks.MvcApplication" Language="C#" %>
---
+++ lostsocks/lostsocks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lostsocks.Startup))]
+namespace lostsocks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
