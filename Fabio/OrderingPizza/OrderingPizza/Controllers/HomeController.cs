9a074e99a6b806d232e5e43ac6ccfe8514c0ed4e
FabioDeNigris <fabio.denigris85@gmail.com>
2015-08-10 12:44:12 +0100

    first task

---
+++ Fabio/OrderingPizza/OrderingPizza/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace OrderingPizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Order()
+        {
+            ViewBag.Message = "Select and order your pizza";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
